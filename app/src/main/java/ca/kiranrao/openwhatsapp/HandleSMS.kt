ef9eb57e88a80c0d39823dd526ef74e44727bb5d
Kiran Rao <kiran_rao@outlook.com>
2020-08-20 10:18:03 -0400

    Make transition nicer

--- app/src/main/java/ca/kiranrao/openwhatsapp/HandleSMS.kt
+++ app/src/main/java/ca/kiranrao/openwhatsapp/HandleSMS.kt
@@ -7,15 +7,16 @@ import androidx.appcompat.app.AppCompatActivity
 class HandleSMS : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        Toast.makeText(applicationContext, "Message sent", Toast.LENGTH_LONG).show();
         val number = intent.data?.schemeSpecificPart
         if (number != null) {
+            Toast.makeText(applicationContext, "Opening $number in WhatsApp", Toast.LENGTH_LONG)
+                .show();
             WhatsAppServices.openWhatsAppMessageThread(this, number)
         } else {
+            Toast.makeText(applicationContext, "Opening WhatsApp", Toast.LENGTH_LONG).show();
             WhatsAppServices.openWhatsAppDefault(this)
         }
-        finish()
+        finishAffinity()
     }
 
 }
