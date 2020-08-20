544982928d89819dd4bf1a11b51bf09e678a5236
Kiran Rao <kiran_rao@outlook.com>
2020-08-19 22:54:36 -0400

    Sample Button

--- app/src/main/java/ca/kiranrao/openwhatsapp/MainActivity.kt
+++ app/src/main/java/ca/kiranrao/openwhatsapp/MainActivity.kt
@@ -1,11 +1,27 @@
 package ca.kiranrao.openwhatsapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    lateinit var openWhatsappButton: Button;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        openWhatsappButton = this.findViewById(R.id.open_whatsapp_button);
+        openWhatsappButton.setOnClickListener {
+            openWhatsApp("4155551234")
+        }
+    }
+
+    private fun openWhatsApp(phone: String) {
+        val url = "https://api.whatsapp.com/send?phone=$phone"
+        val i = Intent(Intent.ACTION_VIEW)
+        i.data = Uri.parse(url)
+        startActivity(i)
     }
 }
